33e98e614919ceb93de602b3a107013f47c39b64
yeobi <kimhyjjang@gmail.com>
2025-05-04 13:25:53 +0900

    [Feat] Project 생성

---
+++ src/main/java/graduation/project/dodutch_server/DoDutchServerApplication.java
@@ -0,0 +1,13 @@
+package graduation.project.dodutch_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoDutchServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoDutchServerApplication.class, args);
+	}
+
+}
