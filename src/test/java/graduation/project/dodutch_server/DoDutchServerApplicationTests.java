c15c027959808249b401be06791c8dccbb6a3469
yeobi <kimhyjjang@gmail.com>
2025-05-04 13:25:53 +0900

    [Feat] Create Project

---
+++ src/test/java/graduation/project/dodutch_server/DoDutchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package graduation.project.dodutch_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoDutchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
